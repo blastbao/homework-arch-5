31199319d102432a61c85684cbb6f06e62055436
qianyukun <dingpamaoo@163.com>
2015-10-11 21:31:54 +0800

    读取文件修改为参数

--- 1/qianyukun/wc.py
+++ 1/qianyukun/wc.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import sys
+file1 = sys.argv[1]
 letters = {"a":'',"b":'',"c":'',"d":'',"e":'',"f":'',"g":'',"h":'',"i":'',"j":'',"k":'',"l":'',"m":'',"n":'',"o":'',"p":'',"q":'',"r":'',"s":'',"t":'',"u":'',"v":'',"w":'',"x":'',"y":'',"z":'',"A":'',"B":'',"C":'',"D":'',"E":'',"F":'',"G":'',"H":'',"I":'',"J":'',"K":'',"L":'',"M":'',"N":'',"O":'',"P":'',"Q":'',"R":'',"S":'',"T":'',"U":'',"V":'',"W":'',"X":'',"Y":'',"Z":'',}
 num_sum = 0
-with open("anaconda-ks.cfg",'r') as f:
+with open(file1,'r') as f:
 	while True:
 		i = f.read(1)
 		if i:
