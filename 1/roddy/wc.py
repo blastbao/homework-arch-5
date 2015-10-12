f313413207617c4e25787ec15af773dfeae8307b
roddyofchina <923401910@qq.com>
2015-10-12 14:53:05 +0800

    第二次更新

--- 1/roddy/wc.py
+++ 1/roddy/wc.py
@@ -1,54 +1,46 @@
 #!/usr/bin/env python
 #coding:utf8
 import sys
+import re
 
 
-#计算字符数函数
-def countback(worddict):
-	'''计算方法:获取循环过来的字典，通过以下方法
-	进行匹配统计，然后返回一个计算值'''
-	words=0
-	for key,value in worddict.iteritems():
-		if value[-1] == "" and value[1] == "":
-			countdata=len([x for x in value if x.strip()])
-			words += countdata
-		else:
-			try:
-				if len(value[-1]) == 0 and not worddict[key-1][-1] == " ":
-					countdata=len([x for x in value if x.strip()])
-					words += countdata
-				if len(value[-1][1]) == 0 and not worddict[key-1][-1] == " ":
-					countdata=len([x for x in value if x.strip()])
-					words += countdata
-				if not [key+1][1] == " " and not len(value[-1]) == 0 :
-					countdata=len([x for x in value if x.strip()])
-					words += countdata - 1
-			except KeyError:
-				countdata=len([x for x in value if x.strip()])
-				words += countdata
 
-	return words
+def countwords(list1,newlist,count):
+	newword1=len([x for x in newlist if x.strip()])
+	if count==1:
+		list1.append(newlist)
+		return newword1
+	else:
+		if newlist[-1] == "" and newlist[0] == "":
+			list1[0]=newlist
+			return newword1
 
+		if not list1[0][-1] == "" and not newlist[0] == "":
+			list1[0]=newlist
+			return newword1-1
+
+		elif  list1[0][-1] == "" and not newlist[0] == "":
+			list1[0]=newlist
+			return newword1
+
+		elif newlist[0] == "":
+			list1[0]=newlist
+			return newword1
 
 def getwords(vfile):
 	with open(vfile,"r+") as f:
-		wordscount=[]
+		list1=[]
 		count=1
-		worddict={}
+		words=0
 		while True:
-			getdata=f.read(1024)
-			if getdata == "":
+			getdata=f.read(50)
+			if getdata=="":
 				break
-			worddictlist = getdata.split("\n")
-			list1=[]
-			for word in worddictlist:
-				newlist=word.split(" ")
-				list1.extend(newlist)
-			worddict[count] = list1
-			count +=1
-		wordscount=countback(worddict)
-	print wordscount 
-
+			newlist = re.split('[ \n\t]',getdata)
+			cc=countwords(list1,newlist,count)
+			words+=cc
+			count+=1
+	print words
 try:
 	filename = sys.argv[1]
 	getwords(filename)
