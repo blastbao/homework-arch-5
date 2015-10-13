2877c0758a5e7901892c6b6a2763be5cab98910f
baxiaoshi <748152983@qq.com>
2015-10-13 19:23:09 +0800

    wc.py update

--- 1/xinpeng/wc.py
+++ 1/xinpeng/wc.py
@@ -3,12 +3,13 @@ import re
 import time 
 read_len = 1 
 end_str = " "
+line_end = '\n'
 count = 0 
 f = open("test.txt")
 def read_file(f, read_len=read_len):
     words = f.read(read_len)
     while words:
-        while not words.endswith(end_str) and words:
+        while not words.endswith(end_str) or words.endswith(line_end):
             str1 = f.read(1)
             'read end break'
             if not str1:
@@ -18,6 +19,8 @@ def read_file(f, read_len=read_len):
         words = f.read(read_len)
 
 for words in read_file(f):
+    print words
     words = re.split(r"(?:\s)\s*", words.strip())
-    count += len(words)
+    if words[0]:
+        count += len(words)
 print count 
