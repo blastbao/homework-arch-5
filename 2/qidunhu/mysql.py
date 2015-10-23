1e91753e69a7bead64c0625c4d16725e86227ab1
qidunhu <qidunhu@126.com>
2015-10-23 16:09:10 +0800

    第二天

---
+++ 2/qidunhu/mysql.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import MySQLdb
+db=MySQLdb.connect("127.0.0.1","reboot123","reboot123","qidunhu")
+cursor=db.cursor()
+cursor.execute("SELECT * FROM user where age > 10")
+data=cursor.fetchone()
+db.close()
