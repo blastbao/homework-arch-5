aee78b0a2fbb8865bf8e960e106d1cdbf76c1aa1
qidunhu <qidunhu@126.com>
2015-10-23 16:49:06 +0800

    update

--- 2/qidunhu/mysql.py
+++ 2/qidunhu/mysql.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
 import MySQLdb
-db=MySQLdb.connect("127.0.0.1","reboot","reboot123","qidunhu")
+db=MySQLdb.connect("127.0.0.1","reboot123","reboot123","qidunhu")
 cursor=db.cursor()
+cursor.execute("insert into user (name,age) values("test",11)"
 cursor.execute("SELECT * FROM user where age > 10")
 data=cursor.fetchone()
 print data
