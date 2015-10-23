7acdd79010f43f20498ba9a20ae7287575964f36
qidunhu <qidunhu@126.com>
2015-10-23 16:16:32 +0800

    update

--- 2/qidunhu/mysql.py
+++ 2/qidunhu/mysql.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import MySQLdb
-db=MySQLdb.connect("127.0.0.1","reboot123","reboot123","qidunhu")
+db=MySQLdb.connect("127.0.0.1","reboot","reboot123","qidunhu")
 cursor=db.cursor()
 cursor.execute("SELECT * FROM user where age > 10")
 data=cursor.fetchone()
