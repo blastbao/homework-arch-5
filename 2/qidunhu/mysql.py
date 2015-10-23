a41f08a5167de58223d26cabdbd9bf641e84b0d8
qidunhu <qidunhu@126.com>
2015-10-23 16:12:44 +0800

    print data

--- 2/qidunhu/mysql.py
+++ 2/qidunhu/mysql.py
@@ -5,4 +5,5 @@ db=MySQLdb.connect("127.0.0.1","reboot123","reboot123","qidunhu")
 cursor=db.cursor()
 cursor.execute("SELECT * FROM user where age > 10")
 data=cursor.fetchone()
+print data
 db.close()
