890fe535f6adbb6798ce673cc7607ddeceedbf74
qianyukun <dingpamaoo@163.com>
2015-11-22 10:21:02 +0800

    http server multi-thread

--- 6/qianyukun/http_img_1.py
+++ 6/qianyukun/http_img_1.py
@@ -28,25 +28,24 @@ class agentD(Daemon):
                 self.q1.put((conn,addr))
         def returnPic():
             while 1:
-                if not self.q1.empty():
-                    (conn, addr) = q1.get()
-                    print "coming", conn, addr
-                    read_data = conn.recv(10000) #读取受到的请求
-                    print read_data
-                    pic_name = read_data.split(" ")[1][1:] #解析GET请求的文件
-                    print pic_name
-                    try:
-                        with file(pic_name) as f: #打开图片文件
-                            pic_content = f.read() #读取文件
-                            length = len(pic_content) #测量文件长度
-                            html_resp = html
-                            html_resp += "%d\r\n\r\n" % (length) #拼接返回的内容长度
-                            print html_resp
-                            html_resp += pic_content #拼接返回内容
-                    except:
-                        html_resp = html404 #如果出错则返回数据为404
-                    conn.send(html_resp) #发送数据
-                    conn.close() #关闭连接
+                (conn, addr) = q1.get()
+                print "coming", conn, addr
+                read_data = conn.recv(10000) #读取受到的请求
+                print read_data
+                pic_name = read_data.split(" ")[1][1:] #解析GET请求的文件
+                print pic_name
+                try:
+                    with file(pic_name) as f: #打开图片文件
+                        pic_content = f.read() #读取文件
+                        length = len(pic_content) #测量文件长度
+                        html_resp = html
+                        html_resp += "%d\r\n\r\n" % (length) #拼接返回的内容长度
+                        print html_resp
+                        html_resp += pic_content #拼接返回内容
+                except:
+                    html_resp = html404 #如果出错则返回数据为404
+                conn.send(html_resp) #发送数据
+                conn.close() #关闭连接
         thread.start_new_thread(acceptConn, ("accept connection thread started",))
         for i in xrange(10):
             t1 = threading.Thread(target=returnPic, )
